8ec3f548cc22f0df12b31b02092f2777394b2319
Jeremy <79330287+jeremyevith@users.noreply.github.com>
2026-06-04 01:22:22 +0800

    release v1.1.3: publish completed inline docs + API-reference README link

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '1.1.2'
+VERSION = '1.1.3'
 DESCRIPTION = 'ID Analyzer API V2 client library, scan and verify global passport, driver license and identification card.'
 
 
